55f78ea70066287b04923dd86d58bcb58a9a2a84
Ethan Spickard <ethanspickard@gmail.com>
2024-01-15 15:48:47 -0500

    Removed unused test code

--- 2024-RobotCode/src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ 2024-RobotCode/src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -147,14 +147,4 @@ public class SwerveSubsystem extends SubsystemBase {
         backRight.setDesiredState(desiredStates[3]);
     }
     
-    //Autohome code, hopefully we can get this to work
-    
-    
-    // public void resetAllWheels() {
-    //     frontLeft.resetEncoders
-    //     frontRight.setEncoderAngle(frontRight.getAbsoluteEncoderRad());
-    //     backLeft.setEncoderAngle(backLeft.getAbsoluteEncoderRad());
-    //     backRight.setEncoderAngle(backRight.getAbsoluteEncoderRad());
-    // }
-    
 }
